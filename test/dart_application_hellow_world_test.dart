c45813f2f89c8384b9993baa414982c3388c43a0
rufaid <rufaidt79@gmail.com>
2023-08-10 13:17:50 +0530

    hellow world

---
+++ test/dart_application_hellow_world_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_application_hellow_world/dart_application_hellow_world.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
